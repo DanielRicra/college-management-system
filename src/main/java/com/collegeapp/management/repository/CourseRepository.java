5ff28a2c149634be0164de8a82ff5811a1d7a503
TSdnl13 <daniel.int@outlook.com>
2022-04-14 23:32:43 -0500

    reposiory: updateCourse method created

--- src/main/java/com/collegeapp/management/repository/CourseRepository.java
+++ src/main/java/com/collegeapp/management/repository/CourseRepository.java
@@ -2,6 +2,14 @@ package com.collegeapp.management.repository;
 
 import com.collegeapp.management.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CourseRepository extends JpaRepository<Course, String> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Course c SET c.name=?1, c.credits=?2, c.description=?3 WHERE c.courseId=?4")
+    int updateCourse(String name, Integer credits, String description, String courseId);
 }
