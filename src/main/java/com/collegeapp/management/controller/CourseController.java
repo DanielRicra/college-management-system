91d123dca62d7967a05d73f9d9fd268908c915c5
TSdnl13 <daniel.int@outlook.com>
2022-04-14 23:29:54 -0500

    get by id, update and delete by id course

--- src/main/java/com/collegeapp/management/controller/CourseController.java
+++ src/main/java/com/collegeapp/management/controller/CourseController.java
@@ -5,11 +5,7 @@ import com.collegeapp.management.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,10 +25,34 @@ public class CourseController {
         return new ResponseEntity<>(courseService.getCourses(), HttpStatus.OK);
     }
 
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable("id") String courseId) {
+        return courseService.getCourseById(courseId)
+                .map(course -> new ResponseEntity<>(course, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping
     public ResponseEntity<Course> saveCourse(@RequestBody Course course) {
         return courseService.saveCourse(course)
                 .map(savedCourse -> new ResponseEntity<>(savedCourse, HttpStatus.CREATED))
                 .orElse(new ResponseEntity<>(HttpStatus.CONFLICT));
     }
+
+    @PutMapping(path = "{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable("id") String courseId,
+                                               @RequestBody Course course) {
+        return courseService.updateCourse(courseId, course)
+                .map(updatedCourse -> new ResponseEntity<>(updatedCourse, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.CONFLICT));
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity deleteCourseById(@PathVariable("id") String courseId) {
+        if (courseService.deleteCourseById(courseId)) {
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
 }
